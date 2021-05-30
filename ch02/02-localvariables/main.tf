ead614efe41514f6f5d6e57c82ef487f666f688e
Mostafa Negim <mostafa.negim@gmail.com>
2021-05-30 11:53:02 +0430

    update

--- ch02/02-localvariables/main.tf
+++ ch02/02-localvariables/main.tf
@@ -1,3 +1,6 @@
+provider "azurerm" {
+  features {}
+}
 locals {
   resource_name = "${var.application_name}-${var.environment_name}-${var.country_code}"
 }
@@ -11,4 +14,14 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method = "Dynamic"
   domain_name_label = "mydomain"
+}
+variable "application_name" {
+    description = "The name of application"  
+}
+variable "environment_name" {
+  description = "The name of environment"
+}
+
+variable "country_code" {
+  description = "The country code (FR-US-...)"
 }
